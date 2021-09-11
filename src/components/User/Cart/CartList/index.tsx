8491643d9c4da0c85c4a97073c2fe242591af4bf
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2021-09-12 05:14:40 +0700

    change order

--- src/components/User/Cart/CartList/index.tsx
+++ src/components/User/Cart/CartList/index.tsx
@@ -24,97 +24,102 @@ const CartList = () => {
   }, [shoppingCart]);
   return (
     <El.Main>
-      <El.list>
-        {shoppingCart.data.length === 0 && (
-          <El.Li>
-            <El.EmptyCart>Keranjang Kosong</El.EmptyCart>
-          </El.Li>
-        )}
-        <El.TableInfo>
-          <thead>
-            <tr>
-              <th>Produk</th>
-              <th>Harga</th>
-              <th>Jumlah</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {shoppingCart.data.map((val) => {
-              const cover = val.Book.BookPicture.filter(
-                (img) => img.type === "COVER"
-              );
-              return (
-                <tr key={val.id}>
-                  <td>
-                    <El.Product>
-                      <El.CoverWrapper>
-                        <div>
-                          <BookCover
-                            url={cover.length > 0 && cover[0].url}
-                            quality={75}
-                            height={75}
-                            width={50}
-                          />
-                        </div>
-                      </El.CoverWrapper>
-                      <El.Info>
-                        <h1>{val.Book.title}</h1>
-                        <h1>{val.Book.Author.name}</h1>
-                      </El.Info>
-                    </El.Product>
-                  </td>
-                  <td>
-                    <El.Price>
-                      <NumberFormat
-                        value={val.Book.price}
-                        displayType={"text"}
-                        thousandSeparator={"."}
-                        decimalSeparator={","}
-                      />
-                    </El.Price>
-                  </td>
-                  <td>
-                    <UpdateCartInput
-                      cartProps={{
-                        cartId: val.id,
-                        amount: val.amount,
-                      }}
-                    />
-                  </td>
-                  <td>
-                    <DeleteCart cartId={val.id} />
-                  </td>
+      {shoppingCart.data.length === 0 && (
+        <El.EmptyCart>Keranjang Kosong</El.EmptyCart>
+      )}
+      {shoppingCart.data.length > 0 && (
+        <div>
+          <El.list>
+            <El.TableInfo>
+              <thead>
+                <tr>
+                  <th>Produk</th>
+                  <th>Harga</th>
+                  <th>Jumlah</th>
+                  <th></th>
                 </tr>
-              );
-            })}
-          </tbody>
-        </El.TableInfo>
-      </El.list>
-      <El.Footer>
-        <El.AmountPrice>
-          <h1>Total Harga</h1>
-          <h1>
-            {`Rp `}
-            <NumberFormat
-              value={amountPrice}
-              displayType={"text"}
-              thousandSeparator={"."}
-              decimalSeparator={","}
-            />
-          </h1>
-        </El.AmountPrice>
-        <El.BtnWrapper>
-          <Button
-            name="PESAN"
-            type="button"
-            onClick={() => {
-              push({ pathname: "/u/order", query: { type: "shoppingcart" } });
-              dispatch({ type: "CLOSE_MENU" });
-            }}
-          />
-        </El.BtnWrapper>
-      </El.Footer>
+              </thead>
+              <tbody>
+                {shoppingCart.data.map((val) => {
+                  const cover = val.Book.BookPicture.filter(
+                    (img) => img.type === "COVER"
+                  );
+                  return (
+                    <tr key={val.id}>
+                      <td>
+                        <El.Product>
+                          <El.CoverWrapper>
+                            <div>
+                              <BookCover
+                                url={cover.length > 0 && cover[0].url}
+                                quality={75}
+                                height={75}
+                                width={50}
+                              />
+                            </div>
+                          </El.CoverWrapper>
+                          <El.Info>
+                            <h1>{val.Book.title}</h1>
+                            <h1>{val.Book.Author.name}</h1>
+                          </El.Info>
+                        </El.Product>
+                      </td>
+                      <td>
+                        <El.Price>
+                          <NumberFormat
+                            value={val.Book.price}
+                            displayType={"text"}
+                            thousandSeparator={"."}
+                            decimalSeparator={","}
+                          />
+                        </El.Price>
+                      </td>
+                      <td>
+                        <UpdateCartInput
+                          cartProps={{
+                            cartId: val.id,
+                            amount: val.amount,
+                          }}
+                        />
+                      </td>
+                      <td>
+                        <DeleteCart cartId={val.id} />
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </El.TableInfo>
+          </El.list>
+          <El.Footer>
+            <El.AmountPrice>
+              <h1>Total Harga</h1>
+              <h1>
+                {`Rp `}
+                <NumberFormat
+                  value={amountPrice}
+                  displayType={"text"}
+                  thousandSeparator={"."}
+                  decimalSeparator={","}
+                />
+              </h1>
+            </El.AmountPrice>
+            <El.BtnWrapper>
+              <Button
+                name="PESAN"
+                type="button"
+                onClick={() => {
+                  push({
+                    pathname: "/u/order",
+                    query: { type: "shoppingcart" },
+                  });
+                  dispatch({ type: "CLOSE_MENU" });
+                }}
+              />
+            </El.BtnWrapper>
+          </El.Footer>
+        </div>
+      )}
     </El.Main>
   );
 };
